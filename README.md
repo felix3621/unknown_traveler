b6a6621aee99ec8681a772d34427e67ed3fd5abe
Boogaloo3621 <fjellvangsander@gmail.com>
2024-01-29 09:14:10 +0100

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# Ideas
+- Tardis
+  - Unlimited count
+  - move through world
+  - engine
+    - range
+    - speed
+  - upgrades
+    - size
+    - available components
+  - research station
+    - get upgrades
+- Travel hub
+  - Sky city
+  - Trading outpost
+- Travelers guild
+- exploration mechanics
+- waypoints/maps
+- expeditions/quests
+- travel gear
---
+++ src/main/java/io/github/felix3621/unknown_traveler/UnknownTraveler.java
@@ -0,0 +1,37 @@
+package io.github.felix3621.unknown_traveler;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
+
+@Mod(UnknownTraveler.MODID)
+public class UnknownTraveler {
+    public static final String MODID = "unknown_traveler";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public UnknownTraveler() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
